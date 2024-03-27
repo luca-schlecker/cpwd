8d4f38cf3a4733752e51c9eacc7ed3d75fbd1eab
Luca Schlecker <luca.schlecker@hotmail.com>
2024-03-27 21:35:28 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::{
+    env::{self, current_dir},
+    path::{Path, PathBuf},
+};
+
+const HOME_ICON: &str = " ";
+const GIT_ICON: &str = "󰊢 ";
+const ROOT_ICON: &str = "";
+
+fn main() {
+    let current = current_dir().unwrap();
+    let anchor = determine_anchor(&current);
+
+    match anchor {
+        Anchor::Home(rel) => println!("{}{}", HOME_ICON, shorten_relative(&rel, false)),
+        Anchor::Git(repo, rel) => println!("{}{}{}", GIT_ICON, repo, shorten_relative(&rel, false)),
+        Anchor::Root(rel) => println!("{}{}", ROOT_ICON, shorten_relative(&rel, true)),
+    }
+}
+
+enum Anchor {
+    Home(PathBuf),
+    Git(String, PathBuf),
+    Root(PathBuf),
+}
+
+fn determine_anchor(p: &Path) -> Anchor {
+    if let Some(git_dir) = get_git_dir(p) {
+        Anchor::Git(
+            git_dir.file_name().unwrap().to_str().unwrap().to_string(),
+            p.strip_prefix(git_dir).unwrap().to_path_buf(),
+        )
+    } else {
+        let home = if cfg!(target_os = "windows") {
+            env::var("USERPROFILE").unwrap()
+        } else {
+            env::var("HOME").unwrap()
+        };
+        let home = Path::new(&home);
+        if let Ok(rel) = p.strip_prefix(home) {
+            Anchor::Home(rel.to_path_buf())
+        } else {
+            Anchor::Root(p.strip_prefix("/").unwrap().to_path_buf())
+        }
+    }
+}
+
+fn shorten_relative(p: &Path, slash_on_empty: bool) -> String {
+    let component_count = p.components().count();
+
+    if component_count == 0 {
+        if slash_on_empty { "/" } else { "" }.to_owned()
+    } else if component_count < 2 {
+        format!("/{}", p.to_str().unwrap().to_owned())
+    } else {
+        format!("/../{}", p.file_name().unwrap().to_str().unwrap())
+    }
+}
+
+fn get_git_dir(mut p: &Path) -> Option<PathBuf> {
+    loop {
+        if p.join(".git").exists() {
+            break Some(p.to_path_buf());
+        }
+        if let Some(parent) = p.parent() {
+            p = parent;
+        } else {
+            break None;
+        }
+    }
+}
